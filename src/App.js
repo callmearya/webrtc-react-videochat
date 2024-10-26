a5c0a29482c654892902df54cc09c39be5fa3f9e
callmearya <60613987+callmearya@users.noreply.github.com>
2024-10-26 17:16:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ import "./App.css";
 
 // Initialize Firebase
 const firebaseConfig = {
-    // YOUR FIREBASE CONFIG HERE
+  apiKey: "AIzaSyD1b7InCyJf03f82MBrFCXNd_1lir3nWrQ",
+  authDomain: "lil-testing.firebaseapp.com",
+  databaseURL: "https://lil-testing-default-rtdb.firebaseio.com",
+  projectId: "lil-testing",
+  storageBucket: "lil-testing.appspot.com",
+  messagingSenderId: "309006701748",
+  appId: "1:309006701748:web:2cfa73093e14fbcc2af3e1"
 };
 
 if (!firebase.apps.length) {
